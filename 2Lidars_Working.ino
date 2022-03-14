15095345772605449152b36e0bf212bab2f3128c
Jack Freeman <47823573+Jofreem@users.noreply.github.com>
2022-03-14 19:31:55 -0400

    Add files via upload
    Most recent arduino codes which run two lidars, change the I2C address (which we have already done for the LiDARs) and test the full movement of the robot.

---
+++ 2Lidars_Working.ino
@@ -0,0 +1,53 @@
+/******************************************************************************
+
+  Code adapted from online
+
+******************************************************************************/
+
+//For this example to work, you must daisy chain together two Qwiic LIDAR sensors with different I2C addresses
+//To change the address of a Qwiic LIDAR sensor, please visit example 2
+
+#include <LIDARLite_v4LED.h> //Click here to get the library: http://librarymanager/All#SparkFun_LIDARLitev4 by SparkFun
+
+//instantiate both LIDARs
+LIDARLite_v4LED myLIDAR1;
+LIDARLite_v4LED myLIDAR3;
+
+void setup() {
+  Serial.begin(115200);
+  while(!Serial);
+  Serial.println("Qwiic LIDARLite_v4 examples");
+  Wire.begin(); //Join I2C bus
+
+  //check if LIDARs will acknowledge over I2C
+  //Connect to Qwiic LIDAR at address 0x5B
+  if (myLIDAR1.begin(0x5B) == false) {
+    Serial.println("LIDAR 1 did not acknowledge! Freezing.");
+    while(1);
+  }
+
+    if (myLIDAR3.begin(0x4A) == false) {
+    Serial.println("LIDAR 3 did not acknowledge! Freezing.");
+    while(1);
+  }
+  Serial.println("All LIDARs acknowledged.");
+}
+
+void loop() {
+  float newDistance1;
+  float newDistance3;
+
+  newDistance1 = myLIDAR1.getDistance();
+  newDistance3 = myLIDAR3.getDistance();
+
+  //Print to Serial port
+  Serial.print("LIDAR 1 distance: ");
+  Serial.print(newDistance1/100);
+  Serial.println(" m");
+
+  Serial.print("LIDAR 3 distance: ");
+  Serial.print(newDistance3/100);
+  Serial.println(" m");
+
+  delay(20); //Don't hammer too hard on the I2C bus
+}
